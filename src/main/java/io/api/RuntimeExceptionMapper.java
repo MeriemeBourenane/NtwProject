50888c41d5008f978ae9b91f5e645751e6e88a63
MeriemeBourenane <merieme.bourenane95@gmail.com>
2021-05-27 22:51:11 +0200

    [ADD] RuntimeExceptionMapper basic exception mapper

---
+++ src/main/java/io/api/RuntimeExceptionMapper.java
@@ -0,0 +1,14 @@
+package io.api;
+
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+
+@Provider
+public class RuntimeExceptionMapper implements ExceptionMapper<RuntimeException> {
+
+    @Override
+    public Response toResponse(RuntimeException e) {
+        return Response.status(400).entity(e.getMessage()).type("plain/text").build();
+    }
+}
